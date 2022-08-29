fa6a940b6af61ab1b6faecd4009b92a873bcced7
Dev0 <dev0@syspulse.io>
2022-08-29 18:42:20 +0300

    Clean up from old flow

--- haas-ingest/ingest-gecko/src/main/scala/io/syspulse/haas/ingest/gecko/App.scala
+++ haas-ingest/ingest-gecko/src/main/scala/io/syspulse/haas/ingest/gecko/App.scala
@@ -10,16 +10,11 @@ import io.syspulse.skel.config._
 
 import scala.concurrent.{Await, ExecutionContext, Future}
 
-import io.syspulse.haas.token.store.TokenStoreElastic
-import io.syspulse.haas.token.store.TokenStoreMem
-import io.syspulse.skel.token.store.TokenStoreStdout
-
 import io.syspulse.haas.ingest.gecko.flow.{ PipelineCoins,PipelineCoinInfo }
 import java.util.concurrent.TimeUnit
 import scala.concurrent.Awaitable
 
 case class Config(  
-  cgUri:String = "",  
   
   feed:String = "",
   output:String = "",
@@ -32,10 +27,6 @@ case class Config(
   
   entity:String = "",
   
-  elasticUri:String = "",
-  elasticUser:String = "",
-  elasticPass:String = "",
-  elasticIndex:String = "",
   expr:String = "",
   
   datastore:String = "",
@@ -57,16 +48,10 @@ object App {
       new ConfigurationProp,
       new ConfigurationEnv, 
       new ConfigurationArgs(args,"ingest-gecko","",
-        ArgString('i', "cg.uri","Coingecko uri"),
                 
         ArgString('f', "feed","Input Feed (def: )"),
         ArgString('o', "output","Output file (pattern is supported: data-{yyyy-MM-dd-HH-mm}.log)"),
         ArgString('e', "entity","Ingest entity: (coin,coins)"),
-
-        ArgString('_', "elastic.uri","Elastic uri (def: http://localhost:9200)"),
-        ArgString('_', "elastic.user","Elastic user (def: )"),
-        ArgString('_', "elastic.pass","Elastic pass (def: )"),
-        ArgString('_', "elastic.index","Elastic Index (def: video)"),
 
         ArgLong('_', "limit","Limit"),
         ArgLong('_', "freq","Frequency"),
@@ -79,18 +64,12 @@ object App {
         ArgString('d', "datastore","datastore [elastic,stdout,file] (def: stdout)"),
         
         ArgCmd("ingest","Ingest"),
-        ArgCmd("ingest-old","Ingest old"),
+        
         ArgParam("<params>","")
       ).withExit(1)
     ))
 
     val config = Config(
-      cgUri = c.getString("cg.uri").getOrElse("http://localhost:8100"),
-      
-      elasticUri = c.getString("elastic.uri").getOrElse("http://localhost:9200"),
-      elasticIndex = c.getString("elastic.index").getOrElse("token"),
-      elasticUser = c.getString("elastic.user").getOrElse(""),
-      elasticPass = c.getString("elastic.pass").getOrElse(""),
       
       feed = c.getString("feed").getOrElse(""),
       output = c.getString("output").getOrElse(""),
@@ -113,16 +92,6 @@ object App {
 
     Console.err.println(s"Config: ${config}")
 
-    val store = config.datastore match {      
-      case "elastic" => new TokenStoreElastic().connect(config.elasticUri,config.elasticIndex)
-      case "mem" => new TokenStoreMem()
-      case "stdout" => new TokenStoreStdout()
-      case _ => {
-        Console.err.println(s"Uknown datastore: '${config.datastore}'")
-        sys.exit(1)
-      }
-    }
-
     config.cmd match {
       case "ingest" => {
         val pp = config.entity match {
@@ -133,7 +102,7 @@ object App {
           case _ =>  Console.err.println(s"Uknown entity: '${config.entity}'"); sys.exit(1)
         } 
 
-        val r = pp.run
+        val r = pp.run()
         r match {
           case a:Awaitable[_] => Await.ready(a,FiniteDuration(30,TimeUnit.MINUTES))
           case akka.NotUsed => 
@@ -143,15 +112,6 @@ object App {
         sys.exit(0)
       }
 
-      // case "ingest-old" => {
-      //   config.entity match {
-      //     case "coins" => 
-      //       new IngestCoins(config,c).run()
-      //     case "coin" => 
-      //       new IngestCoinInfo(config,c).run()
-      //     case _ =>  Console.err.println(s"Uknown entity: '${config.entity}'"); sys.exit(1)
-      //   }
-      // }
     }
   }
 }
