b1031119b787f2551fb4dc8325ef5e6482169704
Dev0 <dev0@syspulse.io>
2022-09-06 15:29:51 +0300

    WIP refactor ABI to sep module

--- haas-ingest/ingest-eth/src/main/scala/io/syspulse/haas/ingest/eth/intercept/InterceptorERC20.scala
+++ haas-ingest/ingest-eth/src/main/scala/io/syspulse/haas/ingest/eth/intercept/InterceptorERC20.scala
@@ -32,58 +32,15 @@ import codegen.Decoder
 import codegen.AbiDefinition
 import os._
 
-case class ERC20Abi(addr:String,abi:Seq[AbiDefinition],funcTo:String, funcValue:String, name:String="") {
-  override def toString = s"${getClass().getSimpleName()}(${addr},${abi.size},${name},${funcTo},${funcValue})"
-}
-
 class InterceptorERC20(config:Config) extends InterceptorTx(config) {
+    
+  val erc20s = AbiRepo.build().withRepo(new AbiRepoFiles(config.abi)).load()
   
-  log.info(s"scanning ABI: ${config.abi}")
-  val abis = os.list(os.Path(config.abi,os.pwd)).flatMap( f => {        
-    val (name,addr) = f.last.split("[-.]").toList match {
-      case name :: addr :: _ => (name,addr.toLowerCase())
-      case name :: Nil => (name,"")
-      case _ => ("","")
-    }
-
-    if(!addr.isEmpty()) {
-      val abi = Decoder.loadAbi(scala.io.Source.fromFile(f.toString).getLines().mkString("\n"))
-      if(abi.size != 0) {
-        log.info(s"${f}: ${abi.size}")
-
-        val functionName = "transfer"
-        // find function names
-        val function = abi.filter(d => d.isFunction).find(_.name.get == functionName)
-        if(function.isDefined) {
-
-          val parToName = function.get.inputs.get(0).name
-          val parValueName = function.get.inputs.get(1).name
-          Some(ERC20Abi( addr, abi, parToName, parValueName, name ))
-
-        } else {
-          log.warn(s"${f}: could not find function: '${functionName}'")
-          None
-        }
-      } else {
-        log.warn(s"${f}: failed to load: ${abi}")
-        None
-      }
-    }
-    else {
-      log.warn(s"${f}: could not determine addr")
-      None
-    }
-  })
-
-  // map of addr -> TokenAbi
-  val erc20s = abis.map(ta => ta.addr -> ta).toMap
-  log.info(s"ABI: ${erc20s}")
-
   override def parseTx(tx:Tx):Map[String,Any] = {
     val token = erc20s.get(tx.toAddress.getOrElse(""))
 
     val (toAddress,value,name) = if(token.isDefined) {    
-      val funcHashSize = "0x12345678".size
+      val funcHashSize = AbiRepo.FUNC_HASH_SIZE
       val funcHash = tx.input.take(funcHashSize)
       val input = tx.input.drop(funcHashSize)
       
