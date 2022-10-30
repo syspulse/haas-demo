d70f2edb96620a3e95216cdf9d2e6042c40bfb0d
Dev0 <dev0@syspulse.io>
2022-10-30 15:43:46 +0200

    Fix mount point

--- haas-ingest/ingest-eth/eth-export-UNI.sh
+++ haas-ingest/ingest-eth/eth-export-UNI.sh
@@ -3,7 +3,7 @@
 START=${1:-10861674}
 END=${2:-10861675}
 
-./eth-export-tokens.sh $START $END | ./run-ingest-eth.sh -e token -o "fs3:///mnt/s3/data/dev/{yyyy}/{MM}/{dd}/token-{HH_mm_ss}.csv" --limit=10000
+#./eth-export-tokens.sh $START $END | ./run-ingest-eth.sh -e token -o "fs3:///mnt/s3/data/dev/{yyyy}/{MM}/{dd}/token-{HH_mm_ss}.csv" --limit=10000
 #./eth-export-tokens.sh $START $END | ./run-ingest-eth.sh -e token -o "fs3:///output/data/dev/ethereum/raw/csv/tokens/{yyyy}/{MM}/{dd}/token-{HH_mm_ss}.csv" --limit=10000
-#./eth-export-tokens.sh $START $END | ./run-ingest-eth.sh -e token -o "fs3:///mnt/s3/data/dev/ethereum/raw/csv/tokens/{yyyy}/{MM}/{dd}/token-{HH_mm_ss}.csv" --limit=10000
+./eth-export-tokens.sh $START $END | ./run-ingest-eth.sh -e token -o "fs3:///mnt/s3/data/dev/ethereum/raw/csv/tokens/{yyyy}/{MM}/{dd}/token-{HH_mm_ss}.csv" --limit=10000
 #./eth-export-tokens.sh $START $END | ./run-ingest-eth.sh -e token stdout:// --limit=10000
