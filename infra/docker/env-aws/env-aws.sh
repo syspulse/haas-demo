a35bac4151da73fd06223088360294f050eb5383
Dev0 <dev0@syspulse.io>
2022-11-18 10:54:42 +0200

    Update with env

---
+++ infra/docker/env-aws/env-aws.sh
@@ -0,0 +1,14 @@
+export SMTP_HOST=email-smtp.eu-west-1.amazonaws.com:587
+export SMTP_USER=AKIAZOOK72NSPEQHWWXN
+export SMTP_PASS=BBcOE8dfCunuc8iMi1ZiIQ5dx1NRdoEcFa44Jkqz24v8
+export SMTP_FROM=no-reply@hacken.cloud
+
+export AWS_ACCOUNT=649502643044
+
+export NOTIFY_EMAIL=a.kashcheyev@hacken.io
+
+export GOOGLE_AUTH_CLIENT_ID=""
+export GOOGLE_AUTH_CLIENT_SECRET=""
+
+export TWITTER_AUTH_CLIENT_ID=""
+export TWITTER_AUTH_CLIENT_SECRET=""
