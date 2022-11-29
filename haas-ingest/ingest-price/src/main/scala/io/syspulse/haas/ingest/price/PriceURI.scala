cdcd7f5d89d92d2246ee5add1bc0019a81082ac7
Dev0 <dev0@syspulse.io>
2022-11-29 19:06:24 +0200

    Fixed uri to real service

--- haas-ingest/ingest-price/src/main/scala/io/syspulse/haas/ingest/price/PriceURI.scala
+++ haas-ingest/ingest-price/src/main/scala/io/syspulse/haas/ingest/price/PriceURI.scala
@@ -7,7 +7,7 @@ price://host:port/api
 case class PriceURI(priceUri:String,apiSuffix:String="") {
   
   def build(prefix:String,host:String,url:String,apiSuffix:String) = {
-    prefix + host + "/" + url + apiSuffix
+    prefix + host + url + apiSuffix
   }
 
   def parse():Option[String] = {
@@ -16,7 +16,7 @@ case class PriceURI(priceUri:String,apiSuffix:String="") {
     priceUri.trim.split("://").toList match {
       case "http" :: host :: Nil => Some(build("http://",host,"/data/pricemultifull",apiSuffix))
       case "cryptocomp" :: host :: Nil => Some(build("http://",host,"/api/",""))
-      case "cryptocomp" :: Nil => Some(build("https","min-api.cryptocompare.com/","/data/pricemultifull",apiSuffix))
+      case "cryptocomp" :: Nil => Some(build("https://","min-api.cryptocompare.com","/data/pricemultifull",apiSuffix))
       case _ => None
     }
   }
