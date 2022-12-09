39d5d95bfd00419dc84bda4b94d6ccf287cdf459
Dev0 <dev0@syspulse.io>
2022-12-09 16:31:01 +0200

    Update s3fs docker option

--- demo/env-aws/env-aws.sh
+++ demo/env-aws/env-aws.sh
@@ -4,6 +4,9 @@ export SMTP_PASS=BBcOE8dfCunuc8iMi1ZiIQ5dx1NRdoEcFa44Jkqz24v8
 export SMTP_FROM=no-reply@hacken.cloud
 
 export AWS_ACCOUNT=649502643044
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_REGION=eu-west-1
 
 export NOTIFY_EMAIL=a.kashcheyev@hacken.io
 
