1959c8c463a2f933c0cad71b527733e2c47f0892
Dev0 <dev0@syspulse.io>
2023-01-20 15:03:51 +0200

    Add large tokens dataset

---
+++ demo/env-aws/env-0.sh
@@ -0,0 +1,19 @@
+export SMTP_HOST=email-smtp.eu-west-1.amazonaws.com:587
+export SMTP_USER=AKIAZOOK72NSPEQHWWXN
+export SMTP_PASS=BBcOE8dfCunuc8iMi1ZiIQ5dx1NRdoEcFa44Jkqz24v8
+export SMTP_FROM=no-reply@hacken.cloud
+
+export AWS_ACCOUNT=649502643044
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_REGION=eu-west-1
+
+export NOTIFY_EMAIL=a.kashcheyev@hacken.io
+
+export GOOGLE_AUTH_CLIENT_ID=""
+export GOOGLE_AUTH_CLIENT_SECRET=""
+
+export TWITTER_AUTH_CLIENT_ID=""
+export TWITTER_AUTH_CLIENT_SECRET=""
+
+export S3_BUCKET=haas-data-dev
