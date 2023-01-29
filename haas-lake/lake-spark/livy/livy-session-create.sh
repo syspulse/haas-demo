9efabc929218d5d5f7594ca0b4e5b3afe9d75cea
Dev0 <dev0@syspulse.io>
2023-01-29 23:10:38 +0200

    Add parametrized session

--- haas-lake/lake-spark/livy/livy-session-create.sh
+++ haas-lake/lake-spark/livy/livy-session-create.sh
@@ -3,7 +3,12 @@
 NAME=${1:-HAAS-SESSION}
 SERVICE_URI=${SERVICE_URI:-http://emr.hacken.cloud:8998}
 
-DATA_1="{\"kind\": \"pyspark\", \"name\":\"${NAME}\"}"
+# params can be read with Python:
+# par1 = spark.conf.get("spark.job.param1")
+# par2 = spark.conf.get("spark.job.param2")
+
+
+DATA_1="{\"kind\": \"pyspark\", \"name\":\"${NAME}\", \"conf\":{\"spark.job.param1\": 100,\"spark.job.param2\": \"UNI\"}}"
 
 r1=`curl -X POST --data "$DATA_1" -H "Content-Type: application/json" ${SERVICE_URI}/sessions`
 
---
+++ haas-lake/lake-spark/livy/test-3.py
@@ -0,0 +1,4 @@
+par1 = spark.conf.get("spark.job.param1")
+par2 = spark.conf.get("spark.job.param2")
+print(par1)
+print(par2)
