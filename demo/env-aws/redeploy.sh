bd389e04cf5d45958007afaef6e036c092bdab57
Dev0 <dev0@syspulse.io>
2023-02-14 21:33:12 +0200

    Add redeploy script

---
+++ demo/env-aws/redeploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOCKER=$1
+
+if [[ $DOCKER = skel-* ]]; then
+   REPO="syspulse"
+else
+   REPO="649502643044.dkr.ecr.eu-west-1.amazonaws.com/syspulse"
+fi
+
+docker-compose stop $DOCKER
+docker-compose rm -f $DOCKER
+docker rmi -f  $REPO/$DOCKER 
+
+echo "Waiting for new image..."
+read
+
+if [ "$?" == "0" ]; then
+   docker-compose up -d $DOCKER
+   docker-compose logs -f  $DOCKER
+fi
+
