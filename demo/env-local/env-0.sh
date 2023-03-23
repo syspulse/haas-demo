575b6dba668170afa0a5d03ba9a14578a1fb9a11
Dev0 <dev0@syspulse.io>
2023-03-23 16:26:28 +0200

    Update env-local to run Topology locally
    [+] nginx not to stop on upstream proxy failure

--- demo/env-local/env-0.sh
+++ demo/env-local/env-0.sh
@@ -20,3 +20,7 @@ export ETH_AUTH_CLIENT_ID="eaf9642f76195dca7529c0589e6d6259"
 export ETH_AUTH_CLIENT_SECRET=
 
 export S3_BUCKET=haas-data-dev
+
+# ==========================================================
+export KAFKA_BROKER=broker-1:9092
+export S3_MOUNT=/mnt/s4
