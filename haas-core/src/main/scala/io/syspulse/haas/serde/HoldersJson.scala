00d8a00ce43624d7f4eb5e09d4924c9211036358
Dev0 <dev0@syspulse.io>
2023-03-25 16:38:21 +0200

    Add forgotten

---
+++ haas-core/src/main/scala/io/syspulse/haas/serde/HoldersJson.scala
@@ -0,0 +1,15 @@
+package io.syspulse.haas.serde
+
+import scala.jdk.CollectionConverters._
+
+import scala.util.Random
+
+import spray.json._
+import DefaultJsonProtocol._
+
+import io.syspulse.haas.core.{Holder,Holders}
+
+object HoldersJson extends DefaultJsonProtocol {
+  implicit val jf_holder = jsonFormat2(Holder.apply _)
+  implicit val jf_holders = jsonFormat3(Holders.apply _)
+}
