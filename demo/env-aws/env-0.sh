b20d9075f71271b769d36e3dd46078aab988c4ab
Dev0 <dev0@syspulse.io>
2023-12-14 16:18:25 +0200

    Update envs

--- demo/env-aws/env-0.sh
+++ demo/env-aws/env-0.sh
@@ -1,14 +1,14 @@
-export SMTP_HOST=email-smtp.eu-west-1.amazonaws.com:587
-export SMTP_USER=AKIAZOOK72NSPEQHWWXN
-export SMTP_PASS=BBcOE8dfCunuc8iMi1ZiIQ5dx1NRdoEcFa44Jkqz24v8
+export SMTP_HOST=
+export SMTP_USER=
+export SMTP_PASS=
 export SMTP_FROM=haas@hacken.cloud
 
-export AWS_ACCOUNT=649502643044
+export AWS_ACCOUNT=
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
 export AWS_REGION=eu-west-1
 
-export NOTIFY_EMAIL=a.kashcheyev@hacken.io
+export NOTIFY_EMAIL=info@hacken.io
 
 export GOOGLE_AUTH_CLIENT_ID=""
 export GOOGLE_AUTH_CLIENT_SECRET=""
@@ -19,5 +19,5 @@ export TWITTER_AUTH_CLIENT_SECRET=""
 export S3_BUCKET=haas-data-dev
 
 # ==========================================================
-export KAFKA_BROKER=broker-1.hacken.dev:9092
+export KAFKA_BROKER=broker-1:9092
 export S3_MOUNT=/mnt/s3
--- demo/env-local/env-0.sh
+++ demo/env-local/env-0.sh
@@ -1,14 +1,14 @@
-export SMTP_HOST=email-smtp.eu-west-1.amazonaws.com:587
-export SMTP_USER=AKIAZOOK72NSPEQHWWXN
+export SMTP_HOST=
+export SMTP_USER=
 export SMTP_PASS=
 export SMTP_FROM=haas@hacken.cloud
 
-export AWS_ACCOUNT=649502643044
+export AWS_ACCOUNT=
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
 export AWS_REGION=eu-west-1
 
-export NOTIFY_EMAIL=a.kashcheyev@hacken.io
+export NOTIFY_EMAIL=info@hacken.io
 
 export GOOGLE_AUTH_CLIENT_ID=""
 export GOOGLE_AUTH_CLIENT_SECRET=""
