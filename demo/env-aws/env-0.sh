61c5e01d083478744362cc86ac00fa46816077ef
Dev0 <dev0@syspulse.io>
2023-12-26 22:49:19 +0200

    Update

--- demo/env-aws/env-0.sh
+++ demo/env-aws/env-0.sh
@@ -1,14 +1,14 @@
 export SMTP_HOST=
 export SMTP_USER=
 export SMTP_PASS=
-export SMTP_FROM=haas@hacken.cloud
+export SMTP_FROM=info@syspulse.io
 
 export AWS_ACCOUNT=
 export AWS_ACCESS_KEY_ID=
 export AWS_SECRET_ACCESS_KEY=
 export AWS_REGION=eu-west-1
 
-export NOTIFY_EMAIL=info@hacken.io
+export NOTIFY_EMAIL=info@syspulse.io
 
 export GOOGLE_AUTH_CLIENT_ID=""
 export GOOGLE_AUTH_CLIENT_SECRET=""
